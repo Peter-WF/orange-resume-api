c32419196e134d6074a9d6d36b0b0c5e47fbb639
王斐 <wangfeia@zbj.com>
2018-01-11 15:34:04 +0800

    init: project init

---
+++ README.md
@@ -0,0 +1,13 @@
+# orange-resume-api
+
+> orange-resume 对应的 api 项目
+
+## 快速上手
+
+``` bash
+# install dependencies
+$ npm install // 如果安装失败尝试将 npm 降至 4.0.2
+
+# serve with hot reload at localhost:3000
+$ npm start
+```
---
+++ app/routes/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express')
+const path = require('path')
+require('nodejs-mock-server')
+
+const utils = require('../utils')
+
+const app = express()
+
+MS.init({
+  app,
+  appDir: path.resolve(__dirname, '../..')
+})
+
+// add intercept
+app.use('/api/*', utils.cors, MS.intercept())
+
+app.options('/api/*', function(req, res) {
+  res.sendStatus(200)
+})
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+})
